e3e2337a5b86b9c2f484cd615be74bf0050ab4df
daikiku10 <soccerdk19.910@gmail.com>
2024-11-09 23:05:43 +0900

    20241109-d

--- paiza/c/challenge/d_190.go
+++ paiza/c/challenge/d_190.go
@@ -7,14 +7,10 @@ import (
 	"strconv"
 )
 
-func ChallengeD_170() {
+func ChallengeD_190() {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
 	inputN := sc.Text()
 	N, _ := strconv.Atoi(inputN)
-	sc.Scan()
-	inputM := sc.Text()
-	M, _ := strconv.Atoi(inputM)
-
-	fmt.Println(N * M)
+	fmt.Println(N * 10)
 }
