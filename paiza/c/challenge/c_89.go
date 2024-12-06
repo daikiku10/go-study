075d7df09054994e897e8ca65753ea521cebf136
daikiku10 <soccerdk19.910@gmail.com>
2024-12-07 06:53:42 +0900

    20241207-c

---
+++ paiza/c/challenge/c_89.go
@@ -0,0 +1,45 @@
+package challenge
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// ストラックアウト
+func ChallengeC_89() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	iArr := strings.Split(sc.Text(), " ")
+	H, _ := strconv.Atoi(iArr[0]) // 高さ
+	W, _ := strconv.Atoi(iArr[1]) // 幅
+
+	out := make([]string, 0, H*W)
+	for i := 0; i < H; i++ {
+		sc.Scan()
+		i2Arr := strings.Split(sc.Text(), "")
+		for _, v := range i2Arr {
+			out = append(out, v)
+		}
+	}
+
+	mato := make([]int, 0, H*W)
+	for i := 0; i < H; i++ {
+		sc.Scan()
+		i3Arr := strings.Split(sc.Text(), " ")
+		for _, v := range i3Arr {
+			i, _ := strconv.Atoi(v)
+			mato = append(mato, i)
+		}
+
+	}
+	cnt := 0
+	for i, v := range out {
+		if v == "o" {
+			cnt = cnt + mato[i]
+		}
+	}
+	fmt.Println(cnt)
+}
